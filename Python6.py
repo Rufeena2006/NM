89a6db3a734be2c9657d854cb62cc2ac2e063d18
Rufeena2006 <nefathrufeena@gmail.com>
2025-04-01 18:10:20 +0530

    Python6.py

--- Python6.py
+++ Python6.py
@@ -1,3 +1,5 @@
 import numpy as np
 import pandas as pd
-sorted_patients=patient_df.sort_values(by="BMI",asc
+sorted_patients=patient_df.sort_values(by="BMI", ascending=False)
+print("Patients sorted by BMI level:")
+print("sorted_patients)
